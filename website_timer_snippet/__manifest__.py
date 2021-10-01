af89cbe6acd15a687dc624a2d48efed67fb3c560
drchudasama1007 <drchudasama644@gmail.com>
2021-10-01 12:35:27 +0530

    [ADD]code added for change the version

--- website_timer_snippet/__manifest__.py
+++ website_timer_snippet/__manifest__.py
@@ -8,7 +8,7 @@
 {
     'name': 'Website Timer Snippet',
     'category': 'Website',
-    'version': '13.0.1',
+    'version': '14.0.1',
     'summary': 'Website Timer Snippet',
 
     'description': """
